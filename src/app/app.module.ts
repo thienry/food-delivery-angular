27f1dbab5b903bb9544111fbe714dcbd9c157d64
thienry <thmoura14@hotmail.com>
2019-03-03 17:10:19 -0300

    Added Spinner Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { RestaurantsComponent } from './components/restaurants/restaurants.component';
-import { RestaurantComponent } from './components/restaurants/restaurant/restaurant.component'
+import { RestaurantComponent } from './components/restaurants/restaurant/restaurant.component';
+import { SpinnerComponent } from './components/spinner/spinner.component'
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { RestaurantComponent } from './components/restaurants/restaurant/restaur
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
