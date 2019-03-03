a880f1877f3b112875be835b78296d4738045202
thienry <thmoura14@hotmail.com>
2019-03-03 18:19:26 -0300

    Added function handleError

--- src/app/components/restaurants/restaurants.service.ts
+++ src/app/components/restaurants/restaurants.service.ts
@@ -3,10 +3,12 @@ import { Http } from "@angular/http";
 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 import { Restaurant } from "./restaurant/restaurant.model";
 
 import { FD_API } from "../../app.api";
+import { ErrorHandler } from "app/app.error-handler";
 
 @Injectable()
 export class RestaurantService {
@@ -15,6 +17,7 @@ export class RestaurantService {
   restaurants(): Observable<Restaurant[]> {
     return this.http
       .get(`${FD_API}/restaurants`)
-      .map(response => response.json());
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 }
