9cf9daccf2ed7d4632f5efd4b3e8bb8350c28a24
thienry <thmoura14@hotmail.com>
2019-03-03 21:20:03 -0300

    Used Pipes Async e Date on reviews component

--- src/app/components/restaurants/restaurants.service.ts
+++ src/app/components/restaurants/restaurants.service.ts
@@ -27,4 +27,11 @@ export class RestaurantService {
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
   }
+
+  reviewsOfRestaurants(id: string): Observable<any> {
+    return this.http
+      .get(`${FD_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
 }
