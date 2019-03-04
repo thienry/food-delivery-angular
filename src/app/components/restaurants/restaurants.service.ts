127d5f86ec60ec063ac8f6263fc79f57eaedac5e
thienry <thmoura14@hotmail.com>
2019-03-03 22:03:57 -0300

    Added pipe currency in menu-item component

--- src/app/components/restaurants/restaurants.service.ts
+++ src/app/components/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
 
 import { Restaurant } from "./restaurant/restaurant.model";
+import { MenuItem } from "../restaurant-detail/menu-item/menu-item.model";
 
 import { FD_API } from "../../app.api";
 import { ErrorHandler } from "app/app.error-handler";
@@ -31,6 +32,13 @@ export class RestaurantService {
   reviewsOfRestaurants(id: string): Observable<any> {
     return this.http
       .get(`${FD_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http
+      .get(`${FD_API}/restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
   }
