1efd47fe18ecdb63d91ad4db25bcacb2b0578609
thienry <thmoura14@hotmail.com>
2019-03-04 19:39:00 -0300

    logic of radio component implemented

--- src/app/components/shared/radio/radio.component.ts
+++ src/app/components/shared/radio/radio.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { RadioOption } from './radio-option.model';
+import { Component, OnInit, Input, forwardRef } from "@angular/core";
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from "@angular/forms";
+
+import { RadioOption } from "./radio-option.model";
 
 @Component({
-  selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  selector: "mt-radio",
+  templateUrl: "./radio.component.html",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
-  @Input() options: RadioOption[]
+export class RadioComponent implements OnInit, ControlValueAccessor {
+  @Input() options: RadioOption[];
 
-  value: any
+  value: any;
+  onChange: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   setValue(value: any) {
-    this.value = value
+    this.value = value;
+    this.onChange(this.value);
   }
 
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {}
+
+  setDisabledState?(isDisabled: boolean): void {}
 }
