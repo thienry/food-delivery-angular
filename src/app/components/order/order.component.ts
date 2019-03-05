a64bbf6cc4dc3d708dfd86aa91ca5d78022446ce
thienry <thmoura14@hotmail.com>
2019-03-04 22:35:24 -0300

    Implemented order logic

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { RadioOption } from "../shared/radio/radio-option.model";
 import { OrderService } from "./order.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
+import { Order, OrderItem } from "./order.model";
 
 @Component({
   selector: "mt-order",
@@ -37,6 +38,19 @@ export class OrderComponent implements OnInit {
   }
 
   itemsValue(): number {
-    return this.orderService.itemsValue()
+    return this.orderService.itemsValue();
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems().map(
+      (item: CartItem) => new OrderItem(item.quantity, item.menuItem.id)
+    );
+
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      console.log(`Compora concluída: ${orderId}`);
+
+      this.orderService.clear();
+    });
+    console.log(order);
   }
 }
--- src/app/components/order/order.service.ts
+++ src/app/components/order/order.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+
+import { Observable } from "rxjs/Observable";
+
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
 
+import { Order } from "./order.model";
+
+import { FD_API } from "../../app.api";
+
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService) {}
+  constructor(private cartService: ShoppingCartService, private http: Http) {}
 
   cartItems(): CartItem[] {
     return this.cartService.items;
@@ -23,6 +31,23 @@ export class OrderService {
   }
 
   itemsValue(): number {
-    return this.cartService.total()
+    return this.cartService.total();
+  }
+
+  clear(): void {
+    this.cartService.clear();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append("ContentType", "application/json");
+
+    return this.http
+      .post(
+        `${FD_API}/orders`,
+        JSON.stringify(order),
+        new RequestOptions({ headers })
+      )
+      .map(response => response.json());
   }
 }
