a9b4cad2348e6b81318477bd28f961e906e81569
thienry <thmoura14@hotmail.com>
2019-03-05 14:22:40 -0300

    Created a shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, LOCALE_ID } from "@angular/core";
 import { HttpModule } from "@angular/http";
 import { RouterModule } from "@angular/router";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { ROUTES } from "./app.routes";
 
@@ -20,13 +19,11 @@ import { MenuItemComponent } from "./components/restaurant-detail/menu-item/menu
 import { ReviewsComponent } from "./components/restaurant-detail/reviews/reviews.component";
 import { ShoppingCartService } from "./components/restaurant-detail/shopping-cart/shopping-cart.service";
 import { OrderComponent } from "./components/order/order.component";
-import { InputComponent } from "./components/shared/input/input.component";
-import { RadioComponent } from "./components/shared/radio/radio.component";
 import { OrderItemsComponent } from "./components/order/order-items/order-items.component";
 import { OrderService } from "./components/order/order.service";
 import { DeliveryCostsComponent } from "./components/order/delivery-costs/delivery-costs.component";
 import { OrderSummaryComponent } from "./components/order-summary/order-summary.component";
-import { RatingComponent } from "./components/shared/rating/rating.component";
+import { SharedModule } from "./components/shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -42,19 +39,15 @@ import { RatingComponent } from "./components/shared/rating/rating.component";
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent,
-    RadioComponent,
     OrderItemsComponent,
     DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule,
-    ReactiveFormsModule
+    SharedModule
   ],
   providers: [
     RestaurantService,
