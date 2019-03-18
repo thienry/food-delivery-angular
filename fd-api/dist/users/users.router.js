cce739b032fa0e2e4fd45e472af0f85542a78efc
thienry <thmoura14@hotmail.com>
2019-03-17 21:38:09 -0300

    Refactoring router.ts

--- fd-api/dist/users/users.router.js
+++ fd-api/dist/users/users.router.js
@@ -3,51 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const router_1 = require("../common/router");
 const users_model_1 = require("./users.model");
 class UsersRouter extends router_1.Router {
+    constructor() {
+        super();
+        this.on("beforeRender", document => {
+            document.password = undefined;
+            //delete document.password
+        });
+    }
     applyRoutes(application) {
         application.get("/users", (req, res, next) => {
-            users_model_1.User.find().then(users => {
-                res.json(users);
-                return next();
-            });
+            users_model_1.User.find().then(this.render(res, next));
         });
         application.get("/users/:id", (req, res, next) => {
-            users_model_1.User.findById(req.params.id).then(user => {
-                if (user) {
-                    res.json(user);
-                    return next();
-                }
-                res.send(404);
-                return next();
-            });
+            users_model_1.User.findById(req.params.id).then(this.render(res, next));
         });
         application.post("/users", (req, res, next) => {
             let user = new users_model_1.User(req.body);
-            user.save().then(user => {
-                user.password = undefined;
-                res.json(user);
-                return next();
-            });
+            user.save().then(this.render(res, next));
         });
         application.put("/users/:id", (req, res, next) => {
-            const options = { overwrite: true };
-            users_model_1.User.update({ _id: req.params.id }, req.body, options)
-                .exec()
-                .then((cmdResult) => {
-                if (cmdResult.result.n) {
-                    return users_model_1.User.findById(req.params.id);
-                }
-                else {
-                    res.send(404);
-                }
-            })
+            users_model_1.User.findByIdAndUpdate({ _id: req.params.id }, req.body)
                 .then(user => {
-                res.json(user);
-                return next();
-            });
-        });
-        application.patch("/users/:id", (req, res, next) => {
-            const options = { new: true };
-            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
                 if (user) {
                     res.json(user);
                     return next();
@@ -56,7 +32,12 @@ class UsersRouter extends router_1.Router {
                     res.send(404);
                     return next();
                 }
-            });
+            })
+                .then(this.render(res, next));
+        });
+        application.patch("/users/:id", (req, res, next) => {
+            const options = { new: true };
+            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(res, next));
         });
         application.del("/users/:id", (req, res, next) => {
             users_model_1.User.remove({ _id: req.params.id })
--- fd-api/users/users.router.ts
+++ fd-api/users/users.router.ts
@@ -3,62 +3,47 @@ import { Router } from "../common/router";
 import { User } from "./users.model";
 
 class UsersRouter extends Router {
+  constructor() {
+    super();
+    this.on("beforeRender", document => {
+      document.password = undefined;
+      //delete document.password
+    });
+  }
+
   applyRoutes(application: restify.Server) {
     application.get("/users", (req, res, next) => {
-      User.find().then(users => {
-        res.json(users);
-        return next();
-      });
+      User.find().then(this.render(res, next));
     });
 
     application.get("/users/:id", (req, res, next) => {
-      User.findById(req.params.id).then(user => {
-        if (user) {
-          res.json(user);
-          return next();
-        }
-        res.send(404);
-        return next();
-      });
+      User.findById(req.params.id).then(this.render(res, next));
     });
 
     application.post("/users", (req, res, next) => {
       let user: any = new User(req.body);
-      user.save().then(user => {
-        user.password = undefined;
-        res.json(user);
-        return next();
-      });
+      user.save().then(this.render(res, next));
     });
 
     application.put("/users/:id", (req, res, next) => {
-      const options = { overwrite: true };
-      User.update({ _id: req.params.id }, req.body, options)
-        .exec()
-        .then((cmdResult: any) => {
-          if (cmdResult.result.n) {
-            return User.findById(req.params.id);
+      User.findByIdAndUpdate({ _id: req.params.id }, req.body)
+        .then(user => {
+          if (user) {
+            res.json(user);
+            return next();
           } else {
             res.send(404);
+            return next();
           }
         })
-        .then(user => {
-          res.json(user);
-          return next();
-        });
+        .then(this.render(res, next));
     });
 
     application.patch("/users/:id", (req, res, next) => {
       const options = { new: true };
-      User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
-        if (user) {
-          res.json(user);
-          return next();
-        } else {
-          res.send(404);
-          return next();
-        }
-      });
+      User.findByIdAndUpdate(req.params.id, req.body, options).then(
+        this.render(res, next)
+      );
     });
 
     application.del("/users/:id", (req, res, next) => {
@@ -66,11 +51,11 @@ class UsersRouter extends Router {
         .exec()
         .then((cmdResult: any) => {
           if (cmdResult.result.n) {
-            res.send(204)
+            res.send(204);
           } else {
-            res.send(404)
+            res.send(404);
           }
-          return next()
+          return next();
         });
     });
   }
