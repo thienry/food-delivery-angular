ecbd37817d7ee974de2d6e312f8b4c44907dbd8b
thienry <thmoura14@hotmail.com>
2019-03-30 14:56:06 -0300

    Added versions at urls

--- fd-api/dist/users/users.router.js
+++ fd-api/dist/users/users.router.js
@@ -5,15 +5,29 @@ const users_model_1 = require("./users.model");
 class UsersRouter extends model_router_1.ModelRouter {
     constructor() {
         super(users_model_1.User);
+        this.findByEmail = (req, res, next) => {
+            if (req.query.email) {
+                users_model_1.User.find({ email: req.query.email })
+                    .then(this.renderAll(res, next))
+                    .catch(next);
+            }
+            else {
+                next();
+            }
+        };
         this.on("beforeRender", document => {
             document.password = undefined;
             //delete document.password
         });
     }
     applyRoutes(application) {
-        application.get("/users", this.findAll);
+        application.get({ path: "/users", version: "2.0.0" }, [
+            this.findByEmail,
+            this.findAll
+        ]);
+        application.get({ path: "/users", version: "1.0.0" }, this.findAll);
         application.get("/users/:id", [this.validateId, this.findById]);
-        application.post("/users", [this.validateId, this.save]);
+        application.post("/users", this.save);
         application.put("/users/:id", [this.validateId, this.replace]);
         application.patch("/users/:id", [this.validateId, this.update]);
         application.del("/users/:id", [this.validateId, this.delete]);
--- fd-api/restaurants/restaurants.router.ts
+++ fd-api/restaurants/restaurants.router.ts
@@ -42,7 +42,7 @@ class RestaurantsRouter extends ModelRouter<Restaurant> {
   applyRoutes(application: restify.Server) {
     application.get("/restaurants", this.findAll);
     application.get("/restaurants/:id", [this.validateId, this.findById]);
-    application.post("/restaurants", [this.validateId, this.save]);
+    application.post("/restaurants", this.save);
     application.put("/restaurants/:id", [this.validateId, this.replace]);
     application.patch("/restaurants/:id", [this.validateId, this.update]);
     application.del("/restaurants/:id", [this.validateId, this.delete]);
